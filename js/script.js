c77df33e8f2f98ea26634c54f17c653ede21129c
Daan Pronk <daanpronk570@gmail.com>
2025-11-13 09:52:48 +0100

    navbar line

--- js/script.js
+++ js/script.js
@@ -69,3 +69,10 @@ particlesJS("particles-js", {
 });
 
 
+    const progressBar = document.getElementById('progressBar');
+    window.addEventListener('scroll', () => {
+      const scrollTop = window.scrollY;
+      const docHeight = document.body.scrollHeight - window.innerHeight;
+      const scrollPercent = (scrollTop / docHeight) * 100;
+      progressBar.style.width = scrollPercent + '%';
+    });
