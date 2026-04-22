133e838ea3649e37f7e94b2996a745b63804c32f
Jesper Niekerk <jespervanniekerk1@gmail.com>
2026-04-22 14:17:39 +0200

    banananana

--- html/public/game/game3.php
+++ html/public/game/game3.php
@@ -1,19 +1,19 @@
 <?php
 $game = [
-    'title' => 'Viking Game',
-    'genre' => "Beat 'em Up",
-    'description' => 'A Viking-themed beat ’em up game where players take on the role of a ruthless Norse warrior, using brutal melee combat to defeat waves of monsters and mythical creatures in intense arenas. Each wave grows more challenging, and players unlock new weapons, abilities, and rage-based special attacks to survive longer and prove their strength.',
-    'image-game' => '../assets/img/thumbnail/team-3.jpg',
-    'image-group' => '../assets/img/group-photo/team-3.jpg',
+    'title' => 'Train Game',
+    'genre' => 'Puzzle',
+    'description' => 'A train-themed puzzle game where players click on objects to complete tasks and unlock the next train car. Each car contains small puzzles that require logical thinking. Once all cars are completed, players reach the conductor, where they must stop the train at the station at the perfect moment.',
+    'image-game' => '../assets/img/thumbnail/team-4.jpg',
+    'image-group' => '../assets/img/group-photo/team-4.jpg',
     'roles' => [
-        'Mylena Krijnen – Game Artist',
-        'Sofija Bashkevich – Game Artist',
-        'Aurora Törlind – Game Artist',
-        'Tyra Hansson – Game Artist',
-        'Kevin Mei – Game Developer',
-        'Sammy Verdan – Game Developer',
-        'Niklas Landborg – Game Developer',
-        'Ture Johansson – Game Developer'
+        'Yevhen Tiutiunnyk – Full Stack Developer',
+        'Julia Heeremans – Game Artist',
+        'Sydney Nieuweveen – Game Artist',
+        'Tim Khysing – Game Artist',
+        'Evelyn Oreland – Game Artist',
+        'Bo Bolweg – Game Developer',
+        'Kalle Ehrsson – Game Developer',
+        'Charlie Buhre Hägglund – Game Developer'
     ],
     'downloadFunction' => 'game3.exe'
 ];
